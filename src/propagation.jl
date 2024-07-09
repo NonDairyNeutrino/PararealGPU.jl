a3604e2e3e3a8b59143f6aa068861b60c6fd4628
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-07-09 16:47:51 -0700

    increase default corrector size by one

--- src/propagation.jl
+++ src/propagation.jl
@@ -8,12 +8,12 @@ end
 
 Propagate an initial value problem using a given propagation scheme.
 """
-function propagate(ivp :: InitialValueProblem, propagator :: Propagator, correctors :: Vector{Float64} = zeros(propagator.discretization)) :: Vector{Vector{Float64}}
+function propagate(ivp :: InitialValueProblem, propagator :: Propagator, correctors :: Vector{Float64} = zeros(propagator.discretization + 1)) :: Vector{Vector{Float64}}
     step              = (ivp.domain.ub - ivp.domain.lb) / propagator.discretization
     discretizedDomain = discretize(ivp.domain, propagator.discretization)
     solution          = similar(discretizedDomain)
     solution[1]       = ivp.initialValue
-    for i in 2 : length(discretizedDomain)
+    for i in Iterators.drop(eachindex(solution), 1)
         solution[i] = propagator.propagator(solution[i - 1], ivp.der(discretizedDomain[i - 1], solution[i - 1]), step) + correctors[i]
     end
     return [discretizedDomain |> collect, solution]
