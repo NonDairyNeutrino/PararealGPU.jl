cfea984d89773f83762df7878cca14ae398ca154
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-07-10 14:28:58 -0700

    move examples directory

--- examples/heat.jl
+++ examples/heat.jl
@@ -3,7 +3,7 @@
 # Author: Nathan Chapman
 # Date: 6/29/24
 
-include("../Parareal.jl")
+include("../src/Parareal.jl")
 using Plots, .Parareal
 
 # DEFINE THE INITIAL VALUE PROBLEM
@@ -16,22 +16,12 @@ const DOMAIN       = Interval(0., 1.)
 const ivp = InitialValueProblem(der, INITIALVALUE, DOMAIN)
 
 # define the underlying propagator e.g. euler, RK4, velocity verlet, etc.
-"""
-    euler(point, slope, step)
-
-The Euler method of numerical integration.
-"""
-function euler(point, slope, step)
-    return point + step * slope
-end
 propagator = euler
 
 # first we must initialize the algorithm with a coarse solution
 # Discretization is the number of sub-domains to use for each time interval
 const COARSEDISCRETIZATION = Threads.nthreads()
-const FINEDISCRETIZATION   = COARSEDISCRETIZATION * 2^2
+const FINEDISCRETIZATION   = 10^2
 
 discretizedDomain, solution = parareal(ivp, propagator, COARSEDISCRETIZATION, FINEDISCRETIZATION)
-display(discretizedDomain)
-display(solution)
-include("../plotting.jl")
+include("../src/plotting.jl")
