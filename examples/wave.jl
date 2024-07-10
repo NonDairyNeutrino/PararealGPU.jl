08986ced097e5a7939f08f0cbd9f4a379aa49b54
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-07-10 16:04:44 -0700

    begin top-down refactoring

--- examples/wave.jl
+++ examples/wave.jl
@@ -7,20 +7,29 @@ include("../src/Parareal.jl")
 using Plots, .Parareal
 
 # DEFINE THE SECOND-ORDER INITIAL VALUE PROBLEM
+"""
+    accerlation(position :: Vector{Float64}, velocity :: Vector{Float64}) :: Vector{Float64}
+
+Define the acceleration given 
+"""
 function accerlation(position :: Vector{Float64}, velocity :: Vector{Float64}) :: Vector{Float64}
     return -position # this encodes the differential equation u''(t) = -u
 end
 
-const INITIALPOSITION = 0
-const INITIALVELOCITY = 0
-const IVP             = # second order initial value problem
+const INITIALPOSITION = 0.
+const INITIALVELOCITY = 0.
+const DOMAIN          = Interval(0., 2 * pi)
+const IVP             = SecondOrderIVP(acceleration, INITIALPOSITION, INITIALVELOCITY, DOMAIN) # second order initial value problem
 
-propagtor = verlet
-
-const COARSEDISCRETIZATION = Threads.nthreads()
+# DEFINE THE COARSE AND FINE PROPAGATION SCHEMES
+const PROPAGATOR           = verlet
+const COARSEDISCRETIZATION = Threads.nthreads()     # 1 region per core
 const FINEDISCRETIZATION   = 2 * Threads.nthreads()
 
-discretizedDomain, discretizedRange = parareal(IVP, propagator, COARSEDISCRETIZATION, FINEDISCRETIZATION)
+const COARSEPROPAGATOR = Propagator(PROPAGATOR, COARSEDISCRETIZATION)
+const FINEPROPAGATOR   = Propagator(PROPAGATOR, FINEDISCRETIZATION)
+
+discretizedDomain, discretizedRange = parareal(IVP, COARSEPROPAGATOR, FINEPROPAGATOR)
 
 # plotting
 include("../src/plotting.jl")
