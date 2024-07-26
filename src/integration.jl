9ffe28354423c07160477d2b8d07f96c3aa7fc58
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-07-25 19:09:19 -0700

    add velocity verlet integrator

--- src/integration.jl
+++ src/integration.jl
@@ -20,3 +20,7 @@ NOTE: This is the base Verlet algorithm, not Stormer-Verlet nor Velocity-Verlet.
 function verlet(position :: Vector{Float64}, prev_position :: Vector{Float64}, acceleration :: Vector{Float64}, step :: Float64) :: Vector{Float64}
     return 2 * position - prev_position + acceleration * step^2
 end
+
+function velocityVerlet(position :: Vector{Float64}, velocity :: Vector{Float64}, step :: Float64)
+    
+end
