622b4fc7a8222e5c2b1c9e20bfec830996474303
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-07-25 21:37:06 -0700

    update to support second order IVP

--- src/propagation.jl
+++ src/propagation.jl
@@ -28,13 +28,12 @@ function propagate(ivp :: SecondOrderIVP, propagator :: Propagator, correctors :
     step              = (ivp.domain.ub - ivp.domain.lb) / propagator.discretization
     discretizedDomain = discretize(ivp.domain, propagator.discretization)
     solution          = similar(discretizedDomain)
+    derivative        = similar(discretizedDomain)
 
     solution[1]       = ivp.initialPosition
-    solution[2]       = ivp.initialPosition + ivp.initialVelocity * step + 0.5 * ivp.acceleration(ivp.initialPosition, ivp.initialVelocity) * step^2 # verlet needs two points
+    derivative[1]     = ivp.initialVelocity
     for i in Iterators.drop(eachindex(solution), 2)
-        # This implementation currently assumes the propagator has the same signature as the "verlet" propagator
-        velocity    = (solution[i - 1] - solution[i - 2]) / step # FIXME: find a better method to get the velocity or find a workaround
-        solution[i] = propagator.propagator(solution[i - 1], solution[i - 2], ivp.acceleration(solution[i], velocity), step) + correctors[i]
+        solution[i], derivative[i] = propagator.propagator(solution[i - 1], derivative[i - 1], ivp.acceleration(solution[i - 1], derivative[i - 1])) + correctors[i]
     end
     return [discretizedDomain |> collect, solution]
 end
