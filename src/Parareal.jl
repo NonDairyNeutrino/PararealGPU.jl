30ad0e5ee71e637222e0d4ead5a11434af6e9390
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-08-01 16:48:28 -0700

    add second order method

--- src/Parareal.jl
+++ src/Parareal.jl
@@ -15,12 +15,12 @@ include("discretization.jl")
 include("propagation.jl")
 
 """
-    parareal(ivp :: T, coarsePropagator :: Propagator, finePropagator :: Propagator) where T <: InitialValueProblem
+    parareal(ivp :: FirstOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
 
 Numerically solve the given initial value problem in parallel using a given
 propagator and discretizations.
 """
-function parareal(ivp :: T, coarsePropagator :: Propagator, finePropagator :: Propagator) where T <: InitialValueProblem
+function parareal(ivp :: FirstOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
     # Some notes on terminology and consistency
     # IVP.................A structure representing an initial value problem
     #                     consisting of a derivative function, an initial value
@@ -45,7 +45,7 @@ function parareal(ivp :: T, coarsePropagator :: Propagator, finePropagator :: Pr
     initialSolution = propagate(ivp, coarsePropagator)
     discretizedDomain, discretizedRange = initialSolution.domain, initialSolution.range
     # create a bunch of smaller initial value problems that can be solved in parallel
-    subProblems = T.(ivp.der, discretizedRange[1:end-1], subDomains) # FIXME: generalize to include SecondOrderIVP
+    subProblems = FirstOrderIVP.(ivp.der, discretizedRange[1:end-1], subDomains) # FIXME: generalize to include SecondOrderIVP
 
     # allocate space
     subSolutionCoarse = similar(subDomains, Solution)
@@ -73,7 +73,72 @@ function parareal(ivp :: T, coarsePropagator :: Propagator, finePropagator :: Pr
         end
         # CORRECTION PHASE
         discretizedRange = propagate(ivp, coarsePropagator, correctors).range
-        subProblems         = T.(ivp.der, discretizedRange[1:end-1], subDomains)
+        subProblems         = FirstOrderIVP.(ivp.der, discretizedRange[1:end-1], subDomains)
+    end
+    return [discretizedDomain, discretizedRange]
+end
+
+"""
+    parareal(ivp :: SecondOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
+
+Numerically solve the given initial value problem in parallel using a given
+propagator and discretizations.
+"""
+function parareal(ivp :: SecondOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
+    # Some notes on terminology and consistency
+    # IVP.................A structure representing an initial value problem
+    #                     consisting of a derivative function, an initial value
+    #                     and a domain (see Domain below)
+    # Domain..............the interval on which an IVP is defined
+    # Subdomain...........a domain that is a subset of another domain
+    # Discretized Domain..a vector of points, all of which are in a domain
+    # Range...............
+    # Discretized Range...a vector of points corresponding to the output of the
+    #                     solution function
+    # Solution............an ordered pair of the discretized domain and the
+    #                     discretized range that satisfies the original IVP
+    # Propagator..........a structure consisting of a numerical integrator
+    #                     and the number of points on which to evaluate
+    coarseDiscretization = coarsePropagator.discretization
+
+    # create a bunch of sub-intervals on which to parallelize
+    subDomains = partition(ivp.domain, coarseDiscretization)
+    # INITIAL PROPAGATION
+    # effectively creating an initial value for each sub-interval
+    # same as the end of the loop but with all correctors equal to zero
+    initialSolution = propagate(ivp, coarsePropagator)
+    discretizedDomain, discretizedRange, discretizedVelocity = initialSolution.domain, initialSolution.range, initialSolution.derivative
+    # create a bunch of smaller initial value problems that can be solved in parallel
+    subProblems = SecondOrderIVP.(ivp.acceleration, discretizedRange[1:end-1], discretizedVelocity[1:end-1], subDomains)
+
+    # allocate space
+    subSolutionCoarse = similar(subDomains, Solution)
+    subSolutionFine   = similar(subDomains, Solution)
+    correctors        = similar(discretizedRange)
+    # LOOP PHASE
+    for iteration in 1:coarseDiscretization # while # TODO: add convergence criterion
+        # println("Iteration $iteration")
+
+        # PARALLEL COARSE
+        Threads.@threads for i in eachindex(subProblems)
+            # println("Coarse subdomain $i is running on thread ", Threads.threadid())
+            subSolutionCoarse[i] = propagate(subProblems[i], coarsePropagator)
+        end
+
+        # PARALLEL FINE
+        Threads.@threads for i in eachindex(subProblems)
+            # println("Fine subdomain $i is running on thread ", Threads.threadid())
+            subSolutionFine[i] = propagate(subProblems[i], finePropagator)
+        end
+
+        # CORRECTORS
+        for i in eachindex(subProblems)
+            correctors[i] = subSolutionFine[i].range[end] - subSolutionCoarse[i].range[end]
+        end
+        # CORRECTION PHASE
+        discretizedRange = propagate(ivp, coarsePropagator, correctors).range
+        
+        subProblems      = SecondOrderIVP.(ivp.acceleration, discretizedRange[1:end-1], discretizedVelocity[1:end-1], subDomains)
     end
     return [discretizedDomain, discretizedRange]
 end
