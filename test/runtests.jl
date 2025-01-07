072c4b53af5244ea3b2f23f864c186e71173b63f
nondairyneutrino <chapmann19@gmail.com>
2025-01-06 23:22:08 -0800

    set up for main loop

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
+using Plots
 
 include("../src/subproblems.jl")
 include("../src/integration.jl")
@@ -10,7 +11,7 @@ include("../src/integration.jl")
 Define the acceleration in terms of the given differential equation.
 """
 function acceleration(position :: Vector{Float64}, velocity :: Vector{Float64}) :: Vector{Float64}
-    return -position # this encodes the differential equation u''(t) = -u
+    return -3^2 * position # this encodes the differential equation u''(t) = -u
 end
 
 const INITIALPOSITION = [0.]
@@ -19,20 +20,27 @@ const DOMAIN         = Interval(0., 2 * pi)
 const IVP      = SecondOrderIVP(DOMAIN, acceleration, INITIALPOSITION, INITIALVELOCITY) # second order initial value problem
 
 # DEFINE THE COARSE AND FINE PROPAGATION SCHEMES
-const PROPAGATOR           = sympecticEuler
+const PROPAGATOR           = #= velocityVerlet =# symplecticEuler
 const COARSEDISCRETIZATION = 2^0 * Threads.nthreads() # 1 region per core
+const FINEDISCRETIZATION   = 2^1 * COARSEDISCRETIZATION
 const COARSEPROPAGATOR = Propagator(PROPAGATOR, COARSEDISCRETIZATION)
+const FINEPROPAGATOR   = Propagator(PROPAGATOR, FINEDISCRETIZATION)
 
 # TESTING
 # println("Testing partition")
-subDomainVector = partition(IVP.domain, COARSEPROPAGATOR.discretization)
+# subDomainVector = partition(IVP.domain, COARSEPROPAGATOR.discretization)
 # display(subDomainVector)
 
 # println("Testing propagate")
-initialSolution = propagate(IVP, COARSEPROPAGATOR)
+# initialSolution = propagate(IVP, COARSEPROPAGATOR)
 # display(initialSolution)
 
 # println("Testing subProblems")
-subProblemVector = createSubproblems(IVP, COARSEPROPAGATOR)
+rootSolution, subProblemVector = initializeSubproblems(IVP, COARSEPROPAGATOR)
 # display(subProblemVector)
+
+subSolutionCoarseVector = similar(subProblemVector, Solution)
+subSolutionFineVector   = similar(subProblemVector, Solution)
+positionCorrectorVector = similar(subProblemVector, Vector{Float64})
+velocityCorrectorVector = similar(subProblemVector, Vector{Float64})
 
