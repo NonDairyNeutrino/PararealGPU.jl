bc6affacc0095056aec76aa0a9e408096aba85dd
nondairyneutrino <chapmann19@gmail.com>
2025-01-07 19:59:15 -0800

    refactor main module

--- src/PararealGPU.jl
+++ src/PararealGPU.jl
@@ -3,87 +3,16 @@ Parareal.jl provides functionality to solve an initial value problem in parallel
 """
 module PararealGPU
 
-export euler, verlet, velocityVerlet           # integration.jl
+export euler, symplecticEuler, velocityVerlet  # integration.jl
 export Interval, FirstOrderIVP, SecondOrderIVP # ivp.jl
 export Propagator
 export parareal                                # Parareal.jl
 
 using CUDA
-import Adapt
 
+include("subproblems.jl")
 include("integration.jl")
-include("ivp.jl")
-include("convergence.jl")
-include("discretization.jl")
-include("propagation.jl")
-include("kernel.jl")
-
-"""
-    parareal(ivp :: FirstOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
-
-Numerically solve the given initial value problem in parallel using a given
-propagator and discretizations.
-"""
-function parareal(ivp :: FirstOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
-    # Some notes on terminology and consistency
-    # IVP.................A structure representing an initial value problem
-    #                     consisting of a derivative function, an initial value
-    #                     and a domain (see Domain below)
-    # Domain..............the interval on which an IVP is defined
-    # Subdomain...........a domain that is a subset of another domain
-    # Discretized Domain..a vector of points, all of which are in a domain
-    # Range...............
-    # Discretized Range...a vector of points corresponding to the output of the
-    #                     solution function
-    # Solution............an ordered pair of the discretized domain and the
-    #                     discretized range that satisfies the original IVP
-    # Propagator..........a structure consisting of a numerical integrator
-    #                     and the number of points on which to evaluate
-    coarseDiscretization = coarsePropagator.discretization
-
-    # create a bunch of sub-intervals on which to parallelize
-    subDomains = partition(ivp.domain, coarseDiscretization)
-    # INITIAL PROPAGATION
-    # effectively creating an initial value for each sub-interval
-    # same as the end of the loop but with all correctors equal to zero
-    initialSolution = propagate(ivp, coarsePropagator)
-    discretizedDomain, discretizedRange = initialSolution.domain, initialSolution.range
-    # create a bunch of smaller initial value problems that can be solved in parallel
-    subProblems = FirstOrderIVP.(ivp.der, discretizedRange[1:end-1], subDomains)
-
-    # allocate space
-    subSolutionCoarse = similar(subDomains, Solution)
-    subSolutionFine   = similar(subDomains, Solution)
-    correctors        = similar(discretizedRange)
-    # LOOP PHASE
-    for iteration in 1:coarseDiscretization # while # TODO: add convergence criterion
-        # println("Iteration $iteration")
-
-        # PARALLEL COARSE
-        kernel = @cuda launch = false pararealKernel(subProblems, coarsePropagator, subSolutionCoarse)
-        config = launch_configuration(kernel.fun)
-        kernel(subProblems, coarsePropagator, subSolutionCoarse; config.threads, config.blocks)
-        # Threads.@threads for i in eachindex(subProblems)
-        #     # println("Coarse subdomain $i is running on thread ", Threads.threadid())
-        #     subSolutionCoarse[i] = propagate(subProblems[i], coarsePropagator)
-        # end
-
-        # PARALLEL FINE
-        Threads.@threads for i in eachindex(subProblems)
-            # println("Fine subdomain $i is running on thread ", Threads.threadid())
-            subSolutionFine[i] = propagate(subProblems[i], finePropagator)
-        end
-
-        # CORRECTORS
-        for i in eachindex(subProblems)
-            correctors[i] = subSolutionFine[i].range[end] - subSolutionCoarse[i].range[end]
-        end
-        # CORRECTION PHASE
-        discretizedRange = propagate(ivp, coarsePropagator, correctors).range
-        subProblems         = FirstOrderIVP.(ivp.der, discretizedRange[1:end-1], subDomains)
-    end
-    return [discretizedDomain, discretizedRange]
-end
+include("correction.jl")
 
 """
     parareal(ivp :: SecondOrderIVP, coarsePropagator :: Propagator, finePropagator :: Propagator)
@@ -106,56 +35,40 @@ function parareal(ivp :: SecondOrderIVP, coarsePropagator :: Propagator, finePro
     #                     discretized range that satisfies the original IVP
     # Propagator..........a structure consisting of a numerical integrator
     #                     and the number of points on which to evaluate
-    
-# TODO: create structs with fields first passed to the device
-# e.g. SecondOrderIVP(ivp.acceleration, cu(discretizedRange[1]), ...)
-    coarseDiscretization = coarsePropagator.discretization
+    rootSolution, subProblemVector = initializeSubproblems(ivp, coarsePropagator)
 
-    # create a bunch of sub-intervals on which to parallelize
-    subDomains = partition(ivp.domain, coarseDiscretization)
-    # INITIAL PROPAGATION
-    # effectively creating an initial value for each sub-interval
-    # same as the end of the loop but with all correctors equal to zero
-    initialSolution = propagate(ivp, coarsePropagator)
-    discretizedDomain, discretizedRange, discretizedVelocity = initialSolution.domain, initialSolution.range, initialSolution.derivative
-    # create a bunch of smaller initial value problems that can be solved in parallel
-    subProblems = SecondOrderIVP.(ivp.acceleration, discretizedRange[1:end-1], discretizedVelocity[1:end-1], subDomains)
+    initialDiscretization        = coarsePropagator.discretization
+    subSolutionCoarseVector = similar(subProblemVector, Solution)
+    subSolutionFineVector   = similar(subProblemVector, Solution)
+    positionCorrectorVector = similar(subProblemVector, Vector{Float64})
+    velocityCorrectorVector = similar(subProblemVector, Vector{Float64})
 
-    # allocate space
-    subSolutionCoarse = similar(subDomains, Solution)
-    subSolutionFine   = similar(subDomains, Solution)
-    rangeCorrectors   = similar(discretizedRange)
-    velocityCorrectors= similar(discretizedRange) # length minus 1?
-    # LOOP PHASE
-    for iteration in 1:coarseDiscretization # while # TODO: add convergence criterion
-        # println("Iteration $iteration")
+    for iteration in 1:initialDiscretization # parareal converges in at most INITIALDISCRETIZATION iterations
+        # the following loops are disjoint to hopefully take advantage of processor pre-fetching
+        # i.e. loop fission
 
-# TODO: change from CPU parallel to GPU parallel with kernel/pararealKernel
-        # PARALLEL COARSE
-
-        # Threads.@threads for i in eachindex(subProblems)
-        #     # println("Coarse subdomain $i is running on thread ", Threads.threadid())
-        #     subSolutionCoarse[i] = propagate(subProblems[i], coarsePropagator)
-        # end
-
-        # PARALLEL FINE
-        Threads.@threads for i in eachindex(subProblems)
-            # println("Fine subdomain $i is running on thread ", Threads.threadid())
-            subSolutionFine[i] = propagate(subProblems[i], finePropagator)
+        # coarse propagation
+        Threads.@threads for i in eachindex(subProblemVector)
+            # this is going to be the CUDA kernel
+            global subSolutionCoarseVector[i] = propagate(subProblemVector[i], coarsePropagator)
         end
 
-        # CORRECTORS
-        for i in eachindex(subProblems)
-            rangeCorrectors[i]    = subSolutionFine[i].range[end]      - subSolutionCoarse[i].range[end]
+        # fine propagation
+        Threads.@threads for i in eachindex(subProblemVector)
+            global subSolutionFineVector[i] = propagate(subProblemVector[i], finePropagator)
         end
-        for i in eachindex(subProblems)
-            velocityCorrectors[i] = subSolutionFine[i].derivative[end] - subSolutionCoarse[i].derivative[end]
+
+        # correction
+        correct!(subSolutionFineVector, subSolutionCoarseVector, positionCorrectorVector, velocityCorrectorVector)
+
+        # correct root solution
+        global rootSolution = propagate(ivp, coarsePropagator, positionCorrectorVector, velocityCorrectorVector)
+
+        # create new sub problems
+        if iteration != initialDiscretization # no need for new subproblems after last iteration
+            updateSubproblems!(subProblemVector, rootSolution, ivp.acceleration)
         end
-        # CORRECTION PHASE
-        corrected   = propagate(ivp, coarsePropagator, rangeCorrectors, velocityCorrectors)
-        discretizedRange, discretizedVelocity = corrected.range, corrected.derivative
-        subProblems = SecondOrderIVP.(ivp.acceleration, discretizedRange[1:end-1], discretizedVelocity[1:end-1], subDomains)
     end
-    return [discretizedDomain, discretizedRange]
+    return rootSolution
 end
 end
