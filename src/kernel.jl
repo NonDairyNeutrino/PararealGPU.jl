a6e7be08885e825817904082b1ffdc8d64ca6aae
nondairyneutrino <chapmann19@gmail.com>
2025-01-08 08:27:10 -0800

    update naming scheme

--- src/kernel.jl
+++ src/kernel.jl
@@ -4,12 +4,12 @@ using CUDA
 
 TBW
 """
-function pararealKernel(subProblems, propagator, subSolution)
+function pararealKernel(subProblemVector, propagator, subSolutionVector)
     index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     stride = gridDim().x * blockDim().x
     # could change below to while loop to avoid StepRangeLength for performance
-    for i = index:stride:length(subProblems)
-        @inbounds subSolution[i] = propagate(subProblems[i], propagator)
+    for i = index:stride:length(subProblemVector)
+        @inbounds subSolutionVector[i] = propagate(subProblemVector[i], propagator)
     end
     return nothing
 end
