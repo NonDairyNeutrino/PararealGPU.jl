3a08ffe881653fa165844d9717c39f514f03303d
nondairyneutrino <chapmann19@gmail.com>
2025-01-12 23:05:34 -0800

    remove base verlet integration

--- src/integration.jl
+++ src/integration.jl
@@ -21,17 +21,6 @@ function symplecticEuler(position :: Vector{T}, velocity :: Vector{T}, accelerat
     return positionNew, velocityNew
 end
 
-# """
-#     verlet(position :: Vector{Float64}, prev_position :: Vector{Float64}, acceleration :: Vector{Float64}, step :: Float64) :: Vector{Float64}
-
-# Get the next position given the current and previous positions, and the current acceleration.
-
-# NOTE: This is the base Verlet algorithm, not Stormer-Verlet nor Velocity-Verlet.
-# """
-# function verlet(position :: Vector{Float64}, prev_position :: Vector{Float64}, acceleration :: Vector{Float64}, step :: Float64) :: Vector{Float64}
-#     return 2 * position - prev_position + acceleration * step^2
-# end
-
 """
     velocityVerlet(position :: Vector{Float64}, velocity :: Vector{Float64}, acceleration :: Function, step :: Float64) :: Vector{Float64}
 
