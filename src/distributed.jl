b6e3f4c11a861bac11ad0b7a6dd8a05812c7021e
nondairyneutrino <chapmann19@gmail.com>
2025-01-23 21:12:10 -0800

    assign device to each process

--- src/distributed.jl
+++ src/distributed.jl
@@ -50,6 +50,32 @@ end
         return -k^2 * position # this encodes the differential equation u''(t) = -u
 end
 # println("acceleration loaded on all processes.")
+
+#= 
+assign device to each process
+- master process (pid 1) which spawns and assigns ids to worker processes on remote hosts
+(one for each device each host).  Each worker process knows its own id.
+- So each host can have multiple worker processes 
+e.g host X has pids 2, 3, and host Y has pids 4, 5, 6
+- Then each process knows the devices (gpus) available to that _host_, but each device has its own
+id _relative to the host_ e.g 
+host X has pids 2, 3 and devices 1, 2, and 
+host Y has pids 4, 5, 6 and devices 1, 2, 3
+
+desired result:
+device 1 on host X to pid 2, 
+device 2 on host X to pid 3, 
+device 1 on host Y to pid 4, 
+etc.
+=#
+# begin loop on master process
+for host in hostVector
+  pidVector = host.pidVector[2:end]
+  devVector = host.devVector
+  for (pid, dev) in zip(pidVector, devVector)
+    println("assigning device $dev to process $pid on host ", host.name)
+    # spawn and fetch task to assign device to process pid
+    remote_do(device!, pid, dev)
     end
 end
 
