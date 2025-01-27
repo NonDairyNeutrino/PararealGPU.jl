58a138386beb5026c9b2b94887c31baf32197ea2
nondairyneutrino <chapmann19@gmail.com>
2025-01-26 22:49:15 -0800

    modularize with spawnManagers

--- src/distributed.jl
+++ src/distributed.jl
@@ -33,49 +33,17 @@ function Base.show(io :: IO, host :: Host)
     println(io, "Devices: ", host.devCount)
 end
 
-remoteHostNameVector = String["Electromagnetism"]
-println("Beginning with remote hosts: ", remoteHostNameVector)
-# create a worker process on each of remote hosts
-subMasterVector = addprocs(remoteHostNameVector) # TODO: port to use topology=:master_worker
+"""
+    spawnManagers(remoteHostNameVector :: Vector{String}) :: Vector{Int}
 
-@everywhere using CUDA # load CUDA module on each process including master
-# hostDeviceCountVector
-hdcVector = pmap(_ -> (gethostname(), ndevices()), subMasterVector) # evals only on workers
-# spawn processes on remote hosts for each device
-addprocs(hdcVector)
-@everywhere using CUDA
-
-hostVector = similar(subMasterVector, Host)
-for i in eachindex(hostVector)
-    name          = remoteHostNameVector[i]
-    pidVector     = procs(subMasterVector[i]) # all pids on same machine as subMasterVector[i]
-    devVector     = 0:hdcVector[i][2]-1 |> collect # devices are indexed at 0
-    hostVector[i] = Host(name, pidVector, devVector)
+Spawn manager processes on each remote host.
+"""
+function spawnManagers(remoteHostNameVector :: Vector{String}) :: Vector{Int}
+    println("Beginning with remote hosts: ", remoteHostNameVector)
+    # create a worker process on each of remote hosts
+    managerVector = addprocs(remoteHostNameVector) # TODO: port to use topology=:master_worker
+    return managerVector
 end
-println("The following hosts, procs, workers, and devices have been automatically recognized.")
-display(hostVector)
-
-# distributed context to each processworkers
-# @everywhere println("Loading from current directory: ", pwd())
-@everywhere include("PararealGPU.jl")
-@everywhere using .PararealGPU
-println("PararealGPU.jl successfully loaded on all processes.")
-
-@everywhere @inline function acceleration(position :: Vector{T}, velocity :: Vector{T}, k = 1) :: Vector{T} where T <: Real
-    return -k^2 * position # this encodes the differential equation u''(t) = -u
-end
-println("acceleration loaded on all processes.")
-
-#= 
-assign device to each process
-- master process (pid 1) which spawns and assigns ids to worker processes on remote hosts
-(one for each device each host).  Each worker process knows its own id.
-- So each host can have multiple worker processes 
-e.g host X has pids 2, 3, and host Y has pids 4, 5, 6
-- Then each process knows the devices (gpus) available to that _host_, but each device has its own
-id _relative to the host_ e.g 
-host X has pids 2, 3 and devices 1, 2, and 
-host Y has pids 4, 5, 6 and devices 1, 2, 3
 
 desired result:
 device 1 on host X to pid 2, 
