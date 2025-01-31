64bcdf825c774d9df3771a2515327921e55997f7
nondairyneutrino <chapmann19@gmail.com>
2025-01-30 19:18:41 -0800

    test distributed functionality

--- examples/wave.jl
+++ examples/wave.jl
@@ -1,37 +1,40 @@
 # This is an example of using the Parareal algorithm to solve the simple
 # initial value problem of d^2 u / dt^2 = -u with u(0) = u0, u'(0) = v0
 # Author: Nathan Chapman
-# Date: 01/07/25
-
-include("../src/PararealGPU.jl")
-using .PararealGPU
 using Plots
-# SETUP
-"""
-    acceleration(position :: Float64, velocity :: Float64) :: Float64
+include("$(pwd())/src/PararealGPU.jl")
+using .PararealGPU
+const nodeVector = String["Electromagnetism"]
+prepCluster(nodeVector)
+# prepCluster(1)
 
-Define the acceleration in terms of the given differential equation.
-"""
-@inline function acceleration(position :: T, velocity :: T) :: T where T
-    return -position # this encodes the differential equation u''(t) = -u
-end
+# # distributed context to each workers
+# @everywhere workers() println("The current working directory on process ", myid(), " is ", pwd())
+# @everywhere include("$(pwd())/src/PararealGPU.jl")
+# @everywhere using .PararealGPU
+# println("PararealGPU.jl successfully loaded on all processes.")
 
-const INITIALPOSITION = [0.]
-const INITIALVELOCITY = [1.]
-const DOMAIN         = Interval(0., 2^2 * pi)
-const IVP      = SecondOrderIVP(DOMAIN, acceleration, INITIALPOSITION, INITIALVELOCITY) # second order initial value problem
+# # DEFINE THE COARSE AND FINE PROPAGATION SCHEMES
+# const INITIALDISCRETIZATION        = Threads.nthreads()
+# const COARSEPROPAGATOR = Propagator(symplecticEuler, INITIALDISCRETIZATION)
+# const FINEPROPAGATOR   = Propagator(velocityVerlet,  2^1 * INITIALDISCRETIZATION)
 
-# DEFINE THE COARSE AND FINE PROPAGATION SCHEMES
-const INITIALDISCRETIZATION        = Threads.nthreads()
-const COARSEPROPAGATOR = Propagator(symplecticEuler, INITIALDISCRETIZATION)
-const FINEPROPAGATOR   = Propagator(velocityVerlet,  2^1 * INITIALDISCRETIZATION)
+# @everywhere @inline function acceleration(position :: Vector{T}, velocity :: Vector{T}, k = 1) :: Vector{T} where T <: Real
+#     return -k^2 * position # this encodes the differential equation u''(t) = -u
+# end
+# const INITIALPOSITION = [0.]
+# const INITIALVELOCITY = [1.]
+# const DOMAIN         = Interval(0., 2^2 * pi)
+# # const IVP      = SecondOrderIVP(DOMAIN, acceleration, INITIALPOSITION, INITIALVELOCITY) # second order initial value problem
 
-rootSolution = parareal(IVP, COARSEPROPAGATOR, FINEPROPAGATOR)
+# ivpVector = [SecondOrderIVP(DOMAIN, (x, v) -> acceleration(x, v; k), INITIALPOSITION, INITIALVELOCITY) for k in 1:nworkers()]
 
-plot(
-    rootSolution.domain,
-    [rootSolution.positionSequence .|> first, rootSolution.velocitySequence .|> first],
-    label = ["position" "velocity"],
-    title = "propagator: $(FINEPROPAGATOR.propagator), discretization: $INITIALDISCRETIZATION"
-)
-savefig("cos.png")
+# solutionVector = pmap(ivp -> parareal(ivp, COARSEPROPAGATOR, FINEPROPAGATOR))
+
+# plot(
+#     rootSolution.domain,
+#     [rootSolution.positionSequence .|> first, rootSolution.velocitySequence .|> first],
+#     label = ["position" "velocity"],
+#     title = "propagator: $(FINEPROPAGATOR.propagator), discretization: $INITIALDISCRETIZATION"
+# )
+# savefig("cos.png")
