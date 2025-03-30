1e4fde8aa434e9bf248187242e719076fe3f110c
nondairyneutrino <chapmann19@gmail.com>
2025-03-30 16:07:33 -0700

    capitalize repo url

--- docs/make.jl
+++ docs/make.jl
@@ -44,5 +44,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/nondairyneutrino/pararealgpu.jl.git"
+    repo = "github.com/NonDairyNeutrino/PararealGPU.jl.git"
 )
