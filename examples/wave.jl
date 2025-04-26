435c45694dc7e72b843caf58360f973833c337ff
nondairyneutrino <chapmann19@gmail.com>
2025-04-26 15:03:32 -0700

    fix some stuff

--- examples/wave.jl
+++ examples/wave.jl
@@ -9,38 +9,45 @@ devPool = prepCluster(nodeVector)
 
 println("Creating initial value problems")
 # DEFINE THE COARSE AND FINE PROPAGATION SCHEMES
-const COARSEPROPAGATOR = Propagator(symplecticEuler, 8192) # how many problems / GPU cores
-const FINEPROPAGATOR   = Propagator(velocityVerlet,  2048) # how many steps on each core / for each problem
+const COARSEDISCRETIZATION = 8
+const FINEDISCRETIZATION   = 2048
+# TODO: change Propagator to closures
+const COARSEPROPAGATOR = Propagator(symplecticEuler, COARSEDISCRETIZATION) # how many problems / GPU cores
+const FINEPROPAGATOR   = Propagator(velocityVerlet,  FINEDISCRETIZATION) # how many steps on each core / for each problem
 
+@everywhere begin
+"""
+    gen_acc(wave_number :: Int = 1) :: Function
 
-# @everywhere function acceleration(position :: Vector{T}, velocity :: Vector{T}; k = 1) :: Vector{T} where T <: Real
-#     return -k^2 * position # this encodes the differential equation u''(t) = -u
-# end
-
-@everywhere function gen_acc(wave_number :: Int = 1)
+Generate an acceleration function based on the given wave number.
+"""
+function gen_acc(wave_number :: Int = 1) :: Function
     function acc(position :: Vector{T}, velocity :: Vector{T}) :: Vector{T} where T <: Real
         return -wave_number^2 * position
     end
     return acc
+end
 end
 
 const INITIALPOSITION = [0.]
 const INITIALVELOCITY = [1.]
 const DOMAIN          = Interval(0., 2^1 * pi)
 
-ivpVector = similar(devPool, SecondOrderIVP)
+ivpVector = Vector{SecondOrderIVP}(undef, length(devPool))
 for k in 1:length(devPool)
     ivpVector[k] = SecondOrderIVP(DOMAIN, gen_acc(k), INITIALPOSITION, INITIALVELOCITY)
 end
 
 solutionVector = solve(COARSEPROPAGATOR, FINEPROPAGATOR, devPool, ivpVector)
+# TODO: write solutions to a file just in case something goes wrong after this
 
+plot(sin, range(DOMAIN.lb, DOMAIN.ub, COARSEDISCRETIZATION + 1), label = "position - true")
 for (k, rootSolution) in enumerate(solutionVector)
     plot!(
         rootSolution.domain,
         [rootSolution.positionSequence .|> first, rootSolution.velocitySequence .|> first],
         label = ["position - $k" "velocity - $k"],
-        title = "discretization: $INITIALDISCRETIZATION    k <= $k"
+        title = "coarse: $COARSEDISCRETIZATION, fine: $FINEDISCRETIZATION"
     )
 end
 println("Plot saved at ", pwd(), "/cos.png")
