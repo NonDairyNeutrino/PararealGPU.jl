effc7d753cbab0da03e92cf17881d4e89f96b718
nondairyneutrino <chapmann19@gmail.com>
2025-05-01 18:40:36 -0700

    fix convergence tests

--- src/convergence.jl
+++ src/convergence.jl
@@ -1,17 +1,21 @@
 # define the distance between solution vectors
 # could either use function distance
-using LinearAlgebra: norm
+# using LinearAlgebra: norm
 
 function hasConverged(oldSolution :: Solution, newSolution :: Solution; threshold = 10^(-10)) :: Bool
     # position convergence test
     oldPositionSequence = oldSolution.positionSequence
     newPositionSequence = newSolution.positionSequence
-    any(norm.(newPositionSequence - oldPositionSequence) .>= threshold) && return false
+
+    position_has_converged = all(v -> maximum(v) <= threshold, newPositionSequence - oldPositionSequence)
+    # any(norm.(newPositionSequence - oldPositionSequence) .>= threshold) && return false
 
     # velocity convergence test
     oldVelocitySequence = oldSolution.velocitySequence
     newVelocitySequence = newSolution.velocitySequence
-    any(norm.(newVelocitySequence - oldVelocitySequence) .>= threshold) && return false
-
-    return true
+    velocity_has_converged = all(v -> maximum(v) <= threshold, newVelocitySequence - oldVelocitySequence)
+    # any(norm.(newVelocitySequence - oldVelocitySequence) .>= threshold) && return false
+    has_converged = position_has_converged && velocity_has_converged
+    # has_converged && printstyled("Parareal has converged.\n", color=:green)
+    return has_converged
 end
