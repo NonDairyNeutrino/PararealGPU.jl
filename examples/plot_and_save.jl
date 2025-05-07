4cf82b29ef31227f615c0d617f34b8d10781451c
nondairyneutrino <chapmann19@gmail.com>
2025-05-07 08:46:47 -0700

    tweak plotting

--- examples/plot_and_save.jl
+++ examples/plot_and_save.jl
@@ -2,17 +2,17 @@ using Plots: plot, plot!, savefig
 # plot true solution
 plot(
     TRUEDOMAIN,
-    [TRUEPOSITION TRUEVELOCITY],
+    [TRUEPOSITION, TRUEVELOCITY],
     label = ["position - true" "velocity - true"]
 )
 plot!(
     solution.domain,
-    [solution.positionSequence .|> first, solution.velocitySequence .|> first],
+    [solution.positionSequence .|> only, solution.velocitySequence .|> only],
     label = ["position" "velocity"],
     title = "coarse: $COARSEDISCRETIZATION, fine: $FINEDISCRETIZATION"
 )
 plot_dir  = string(pwd(), "/examples/images/")
-plot_name = "wave_single_$(COARSEDISCRETIZATION)_$FINEDISCRETIZATION.pdf"
+plot_name = "wave_single_cd$(COARSEDISCRETIZATION)_fd$(FINEDISCRETIZATION)_ub$(DOMAINUPPERBOUND/pi)pi.pdf"
 plot_abs_path = plot_dir * plot_name
 println("Plot saved at ", plot_abs_path)
 savefig(plot_abs_path)
