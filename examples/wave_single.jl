3533545fe184641e46df97ada5daa702aecbd275
nondairyneutrino <chapmann19@gmail.com>
2025-05-07 11:28:06 -0700

    tweak wave_single example

--- examples/wave_single.jl
+++ examples/wave_single.jl
@@ -9,17 +9,17 @@ using .PararealGPU
 const NODEVECTOR           = String["Electromagnetism"]
 # DEFINE COMPUTATIONAL PARAMETERS
 const COARSEINTEGRATOR     = symplecticEuler
-const COARSEDISCRETIZATION = 2^10                        # how many total problems
+const COARSEDISCRETIZATION = 2^6                        # how many total problems
 const FINEINTEGRATOR       = velocityVerlet
-const FINEDISCRETIZATION   = 2^6                         # each step is 2^7 = 128 ~ 1% of the domain
+const FINEDISCRETIZATION   = 2^8                         # 2^7 = 128 steps -> each step is ~1% of the domain
 # DEFINE MODEL PARAMETERS
-const WAVENUMBER           = 1.0f0 * pi
+const WAVENUMBER           = 1.0f0 # * pi
 # ACCELERATION(r, v)         = -WAVENUMBER^2 * r         # simple harmonic oscillator
-const DOMAINLOWERBOUND, DOMAINUPPERBOUND = 0.0f0, 10.0f0
+const DOMAINLOWERBOUND, DOMAINUPPERBOUND = 0.0f0, 20.0f0 * pi
 const INITIALPOSITION      = Float32[0.]
 const INITIALVELOCITY      = Float32[1.]
 
-const TRUEDOMAIN                 = range(DOMAINLOWERBOUND, DOMAINUPPERBOUND, COARSEDISCRETIZATION + 1) |> collect
+const TRUEDOMAIN                 = range(DOMAINLOWERBOUND, DOMAINUPPERBOUND, COARSEDISCRETIZATION + 1)
 const TRUEPOSITION, TRUEVELOCITY = sin.(WAVENUMBER .* TRUEDOMAIN), cos.(WAVENUMBER .* TRUEDOMAIN)
 
 solution = solve(
