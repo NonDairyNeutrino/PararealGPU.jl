d59b86e9e0cdea55d6091b9c7beb08e5d18c9f12
nondairyneutrino <chapmann19@gmail.com>
2025-08-28 17:37:37 -0700

    automate error plotting

--- analysis/energy.jl
+++ analysis/energy.jl
@@ -10,8 +10,6 @@ include("$(pwd())/src/PararealGPU.jl")
 using .PararealGPU
 include("analysis.jl")
 
-const DATADIR = "../benchmarks/data/"
-
 # DEFINE MODEL PARAMETERS
 # the frequency (spatial or temporal) constrains the potential values for the length of the rod
 # because we're leaving the frequency at 1, and assuming we're on Earth, the length of the rod must
@@ -25,8 +23,18 @@ const INITIALVELOCITY      = Float32[1.]
 # in this case the initial energy is just the kinetic energy because the bob is at the bottom
 # energy in terms of mass, will get canceled when comparing to simulated
 # and I don't want to write the potential energy
-const INITIALENERGY = (1//2) * MASS * sum(abs2, INITIALVELOCITY) # = ||v||^2
+"""
+    0.5 * MASS * sum(abs2, INITIALVELOCITY)
 
+The initial energy of the simulation
+"""
+const INITIALENERGY = 0.5 * MASS * sum(abs2, INITIALVELOCITY) # = ||v||^2
+
+"""
+    plot_energy_time(bench_file :: String, coarse :: Int, fine :: Int; drawpotential = false, drawkinetic = false)
+
+Calculate and plot the energy over the simulated time.
+"""
 function plot_energy_time(bench_file :: String, coarse :: Int, fine :: Int; drawpotential = false, drawkinetic = false)
     bench      = load_bench(bench_file, coarse, fine)
     sol        = bench.value[1]
@@ -65,15 +73,101 @@ function plot_energy_time(bench_file :: String, coarse :: Int, fine :: Int; draw
     return plt
 end
 
-function plot_error_disc(bench_file :: String, coarse :: Int, fine :: Int)
-    bench_dist = load(bench_file; nested = true)
-    for coarse in keys(bench_dist)
-        bench_coarse = bench_dist[coarse]
-        for fine in keys(bench_coarse)
-            bench = bench_coarse[fine]
-            # TODO: calculate error here
-        end
-    end
-    sol   = bench.value[1]
-    calculate_error(INITIALENERGY, sol)
+"""
+    get_errors(bench_dict :: Dict; initial_energy = INITIALENERGY) :: Tuple{Vector{Int}, Vector{Float64}}
+
+Get the discretization and error for a given set of benchmarks.
+"""
+function get_errors(bench_dict :: Dict; initial_energy = INITIALENERGY) :: Tuple{Vector{Int}, Vector{Float64}}
+    disc_vec  = (k -> 2^parse(Int, k)).(keys(bench_dict))
+    sol_vec   = (b -> b.value[1]).(values(bench_dict))
+    err_vec   = calculate_error(initial_energy).(sol_vec)
+
+    ordering  = sortperm(disc_vec)
+    disc_vec .= disc_vec[ordering]
+    err_vec  .= err_vec[ordering]
+
+    return disc_vec, err_vec
 end
+
+"""
+    plot_error_disc(bench_dict :: Dict; initial_energy = INITIALENERGY)
+
+TBW
+"""
+function plot_error_disc(bench_dict :: Dict; initial_energy = INITIALENERGY)
+    disc_vec, err_vec = get_errors(bench_dict; initial_energy = initial_energy)
+    plt = plot(
+        disc_vec,
+        err_vec,
+        xticks = disc_vec,
+        xscale = :log2,
+        xlabel = L"N_\mathcal{F}",
+        ylabel = L"(E - E_0) / E_0",
+        labels = false
+    )
+    return plt
+end
+
+"""
+    plot_error_disc(bench_file :: String, coarse :: Int, fine :: Colon; initial_energy = INITIALENERGY)
+
+TBW
+"""
+function plot_error_disc(bench_file :: String, coarse :: Int, fine :: Colon; initial_energy = INITIALENERGY)
+    fine_bench_dict = load(bench_file; nested = true)["$coarse"]
+    plt = plot_error_disc(fine_bench_dict; initial_energy = initial_energy)
+    return plt
+end
+
+function plot_error_disc(bench_file :: String, coarse :: Colon, fine :: Int)
+    
+end
+
+"""
+    plot_error_disc(bench_file :: String, coarse_vec :: Vector{Int}, fine :: Colon; initial_energy = INITIALENERGY)
+
+TBW
+"""
+function plot_error_disc(bench_file :: String, coarse_vec :: Vector{Int}, fine :: Colon; initial_energy = INITIALENERGY)
+    all_benches         = load(bench_file; nested = true)
+    fine_bench_dict_vec = getindex.(Ref(all_benches), string.(coarse_vec))
+    disc_err_tup_vec    = get_errors.(fine_bench_dict_vec; initial_energy = initial_energy)
+    disc_mat            = first.(disc_err_tup_vec) |> stack
+    err_mat             = Base.Fix2(getindex, 2).(disc_err_tup_vec) |> stack
+    plt                 = plot(
+        disc_mat, 
+        err_mat,
+        xticks = disc_mat[:, 1],
+        xscale = :log2,
+        xlabel = L"N_\mathcal{F}",
+        ylabel = L"(E - E_0) / E_0",
+        labels = (c -> latexstring("N_\\mathcal{C} = $c")).(permutedims(coarse_vec))
+    )
+    return plt
+end
+
+function plot_error_finedisc(bench_file :: String; initial_energy = INITIALENERGY)
+    coarse_bench_dict   = load(bench_file; nested = true)
+    coarse_disc_vec     = keys(coarse_bench_dict)   |> collect .|> Base.Fix1(parse, Int)
+    fine_bench_dict_vec = values(coarse_bench_dict) |> collect
+    ordering            = sortperm(coarse_disc_vec)
+    coarse_disc_vec     = coarse_disc_vec[ordering]
+    fine_bench_dict_vec = fine_bench_dict_vec[ordering]
+
+    disc_err_tup_vec    = get_errors.(fine_bench_dict_vec; initial_energy = initial_energy)
+    disc_mat            = first.(disc_err_tup_vec) |> stack
+    err_mat             = Base.Fix2(getindex, 2).(disc_err_tup_vec) |> stack
+    plt                 = plot(
+        disc_mat, 
+        err_mat,
+        xticks = disc_mat[:, 1],
+        xscale = :log2,
+        xlabel = L"N_\mathcal{F}",
+        ylabel = L"(E - E_0) / E_0",
+        labels = (c -> latexstring("N_\\mathcal{C} = $c")).(permutedims(coarse_disc_vec))
+    )
+    return plt
+end
+
+plot_error_disc(bench_file :: String, coarse :: Colon, fine :: Colon) = plot_error_finedisc(bench_file)
